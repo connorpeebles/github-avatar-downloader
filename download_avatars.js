5375644b1f9e8b774ae5f4bb57ab8abec27ec0f9
Connor Peebles <connorpeebles93@gmail.com>
2018-10-01 20:45:39 +0000

    added headers to the request

--- download_avatars.js
+++ download_avatars.js
@@ -1,12 +1,19 @@
 var request = require("request");
+var secrets = require("./secrets");
 
 console.log("Welcome to the Github Avatar Downloader!");
 
 function getRepoContributors(repoOwner, repoName, cb) {
 
-  var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      "User-Agent": "request",
+      "Authorization": secrets.GITHUB_TOKEN
+    }
+  };
 
-  request(url, function(err, res, body) {
+  request(options, function(err, res, body) {
     cb(err, body);
   });
 
