73ed5b27685ca28f3a58984ee3d04288c73131e2
Connor Peebles <connorpeebles93@gmail.com>
2018-10-01 21:57:11 +0000

    Added support line arguments and made them required

--- download_avatars.js
+++ download_avatars.js
@@ -2,6 +2,8 @@ var request = require("request");
 var fs = require("fs");
 var secrets = require("./secrets");
 
+var args = process.argv;
+
 console.log("Welcome to the Github Avatar Downloader!");
 
 function getRepoContributors(repoOwner, repoName, cb) {
@@ -10,26 +12,22 @@ function getRepoContributors(repoOwner, repoName, cb) {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
       "User-Agent": "request",
-      "Authorization": secrets.GITHUB_TOKEN
+      "Authorization": "token " + secrets.GITHUB_TOKEN
     }
   };
 
   request(options, function(err, res, body) {
+
+    if (err) {
+      console.log("The following error occurred: ", err)
+      throw err;
+    }
+
     var obj = JSON.parse(body);
     cb(err, obj);
   });
 
 }
-
-getRepoContributors("jquery", "jquery", function(err, result) {
-  for (var obj of result) {
-    var filePath = "avatars/" + obj.login;
-    var url = obj.avatar_url;
-    console.log("filepath: ", filePath)
-    console.log("url: ", url)
-    downloadImageByURL(url, filePath);
-  }
-});
 
 function downloadImageByURL(url, filePath) {
 
@@ -39,5 +37,26 @@ function downloadImageByURL(url, filePath) {
          })
          .pipe(fs.createWriteStream(filePath));
 }
+
+getRepoContributors(args[2], args[3], function(err, result) {
+
+  if (args.length < 4) {
+    console.log("Error: please indicate the repoOwner and repoName")
+    return;
+  }
+
+  if (err) {
+    console.log("The following error occurred: ", err)
+    throw err;
+  }
+
+  for (var obj of result) {
+    var filePath = "avatars/" + obj.login;
+    var url = obj.avatar_url;
+    downloadImageByURL(url, filePath);
+  }
+});
+
+
 
 // downloadImageByURL("https://sytantris.github.io/http-examples/future.jpg", "./future.jpg");
